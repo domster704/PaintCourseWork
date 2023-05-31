4c57755dada91815909f533bc4654441fadb8411
domster704 <domster704@mail.ru>
2023-05-31 20:50:25 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "library/paint/header/paint.hpp"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
